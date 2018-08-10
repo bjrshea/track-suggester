c11c0456ec16de69f481e7b9a719986bea4e5da6
Brendan Shea <padma@email.com>
2018-08-10 12:23:11 -0700

    Start building logic with branching.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
-  
+  $("button#track-result").click(function(event) {
 
+    var js = $("input:radio[name=[question1]:checked").val();
+    var ruby = $("input:radio[name=[question2]:checked").val();
+    var csharp = $("input:radio[name=[question3]:checked").val();
+
+  });
 )};
