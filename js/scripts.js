2dd9cf42523cb2fd53181860ab3329b7a22a11d1
Brendan Shea <padma@email.com>
2018-08-10 13:28:48 -0700

    Create logic for javascript alert.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
-  $("button#track-result").click(function(event) {
+  $("#form-1").submit(function(event) {
+    event.preventDefault();
 
-    var js = $("input:radio[name=[question1]:checked").val();
-    var ruby = $("input:radio[name=[question2]:checked").val();
-    var csharp = $("input:radio[name=[question3]:checked").val();
+    var uno = $("input:radio[name=question1]:checked").val();
+    var dos = $("input:radio[name=question2]:checked").val();
+    var tres = $("input:radio[name=question3]:checked").val();
+    var quatro = $("input:radio[name=question4]:checked").val();
+    var cinco = $("input:radio[name=question5]:checked").val();
+
+    if(uno === "answer1" && dos === "answer4" && tres === "answer7") {
+      alert("JavaScript")
+    }
 
   });
-)};
+});
