6ad1b99b24f7034f43bd937ef846331bbde3b68a
Brendan Shea <padma@email.com>
2018-08-15 21:37:40 -0700

    Clean up syntax issues and change variable/radio question names.

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,59 @@
 $(document).ready(function(){
   $("#form-1").submit(function(event) {
-    var uno = $("input:radio[name=question1]:checked").val();
-    var dos = $("input:radio[name=question2]:checked").val();
-    var tres = $("input:radio[name=question3]:checked").val();
-    var quatro = $("input:radio[name=question4]:checked").val();
-    var cinco = $("input:radio[name=question5]:checked").val();
-    var js = 0
-    var ruby = 0
-    var sharp = 0
-    
+    event.preventDefault();
+    var freelance = $("input:radio[name=freelance]:checked").val();
+    var preference = $("input:radio[name=preference]:checked").val();
+    var favorite = $("input:radio[name=favorite]:checked").val();
+    var soundsBest = $("input:radio[name=sounds-best]:checked").val();
+    var feelsRight = $("input:radio[name=feels-right]:checked").val();
+    var js = 0;
+    var ruby = 0;
+    var sharp = 0;
+
     if(uno === "answer1") {
-      js += 1
+      js += 1;
     }
     if(uno === "answer2") {
-      ruby += 1
+      ruby += 1;
     }
     if(uno === "answer3") {
-      sharp += 1
+      sharp += 1;
     }
     if(dos === "answer4") {
-      js += 1
+      js += 1;
     }
     if(dos === "answer5") {
-      ruby += 1
+      ruby += 1;
     }
     if(dos === "answer6") {
-      sharp += 1
+      sharp += 1;
     }
     if(tres === "answer7") {
-      js += 1
+      js += 1;
     }
     if(tres === "answer8") {
-      ruby += 1
+      ruby += 1;
     }
     if(tres === "answer9") {
-      sharp += 1
+      sharp += 1;
     }
     if(quatro === "answer10") {
-      js += 1
+      js += 1;
     }
     if(quatro === "answer11") {
-      ruby += 1
+      ruby += 1;
     }
     if(quatro === "answer12") {
-      sharp += 1
+      sharp += 1;
     }
     if(cinco === "answer13") {
-      js += 1
+      js += 1;
     }
     if(cinco === "answer14") {
-      ruby += 1
+      ruby += 1;
     }
     if(cinco === "answer15") {
-      sharp += 1
+      sharp += 1;
     }
     if(js >= 3) {
       alert("JavaScript/React")
@@ -66,7 +67,6 @@ $(document).ready(function(){
     if(sharp, ruby, js <= 2) {
       alert("It looks like you're on the fence between two tracks! Please try again. :-)")
     }
-    event.preventDefault();
   });
 });
 
